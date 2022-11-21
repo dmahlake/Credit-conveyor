a17bec641f28da171e9e16e35142bbb03451778f
dipuo <dmahlake@student.wethinkcode.com>
2022-11-21 21:27:55 +0200

    modified

--- credit-conveyor/Deal/src/main/java/com/deals/feignClient/ClientSide.java
+++ credit-conveyor/Deal/src/main/java/com/deals/feignClient/ClientSide.java
@@ -8,12 +8,10 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.List;
 
-@FeignClient(url = "https://localhost:8080")
+@FeignClient(url = "${api.url}")
 public interface ClientSide {
 
     @PostMapping("/offers")
